e0f514155fab9c761c87b4e7b2fe3bb7ce771868
Whitney Zilton <ziltonw@oregonstate.edu>
2023-01-29 15:31:28 -0800

    first sprint done

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os
-
-from flask import Flask, render_template
-# from petpy import Petfinder
+from flask import Flask, render_template, request
+from petpy import Petfinder
+import constants
 
 app = Flask(__name__, template_folder='templates')
 
@@ -18,14 +18,36 @@ def about_me():
     return render_template('about.html')
 
 
-@app.route('/search-results')
+@app.route('/search-results', methods=['GET'])
 def search_results():
     """Page showing results for pets available for adoption"""
-    return render_template('search_results.html')
+
+    pf = Petfinder(key=constants.API_KEY,
+                   secret=constants.API_SECRET)
+
+    # getting data from html form
+    pet_type = request.args.get('type')
+    zipcode = request.args.get('zipcode')
+    distance = int(request.args.get('distance'))
+
+    # sending request to petfinder api
+    response = pf.animals(animal_type=pet_type, location=zipcode, distance=distance, results_per_page=12)
+
+    # response from api
+    animal_list = []
+    for i in range(len(response["animals"])):
+        animal_dict = dict()
+        animal_dict['name'] = response["animals"][i]['name']
+        animal_dict['url'] = response["animals"][i]['url']
+        animal_dict['gender'] = response["animals"][i]['gender']
+        animal_dict['age'] = response["animals"][i]['age']
+        animal_dict['description'] = response["animals"][i]['description']
+        animal_list.append(animal_dict)
+
+    return render_template('search_results.html', animals=animal_list)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 3800))
-    print("this is the port number found: ", port)
     app.run(port=port)
     # app.run(debug=True, host='0.0.0.0')
